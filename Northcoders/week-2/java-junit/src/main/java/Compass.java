10752d971b01fb5c25415ae2d27fd7b9515f6314
JannelJ <janneljones26@gmail.com>
2024-07-29 11:09:38 +0100

    Add enums and methods for testing

---
+++ Northcoders/week-2/java-junit/src/main/java/Compass.java
@@ -0,0 +1,17 @@
+public class Compass {
+
+    public enum Point {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST,
+    }
+    public enum Direction {
+        LEFT,
+        RIGHT,
+    }
+    String point;
+    String rotate(String point, String direction) {
+        return null;
+    }
+}
